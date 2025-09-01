1189d9c18572797c26c0842219ff2f207cdefe3c
Jake <jweststewart@gmail.com>
2025-09-01 13:06:29 -0600

    removed unneeded files and updated readme and concepts

--- memory/__init__.py
+++ memory/__init__.py
@@ -1,6 +1,5 @@
 """Memory system for 5W1H framework"""
 
-from .hopfield import ModernHopfieldNetwork
 from .memory_store import MemoryStore
 
-__all__ = ['ModernHopfieldNetwork', 'MemoryStore']
+__all__ = ['MemoryStore']
